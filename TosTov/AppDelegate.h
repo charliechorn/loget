7ef071057f402ae75a14829749ecbc58088ec9d1
gaeasysadmin <gaeasysadmin@macbook-pro-2.local>
2016-11-02 14:59:09 +0700

    Initial Commit

---
+++ TosTov/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TosTov
+//
+//  Created by Pichzz on 11/2/16.
+//  Copyright © 2016 Chhaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TosTov/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TosTov
+//
+//  Created by Pichzz on 11/2/16.
+//  Copyright © 2016 Chhaly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
